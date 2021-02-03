0d71d784b8571c36ce0608d43765cd083e89b2f4
Enrique Puyol <kpuyol94@hotmail.com>
2021-02-03 21:59:24 +0100

    Added a test scenario level
    + Sniper anims
    + Highrank anims

---
+++ Project WW2/Assets/Scripts/Terrains.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrains : MonoBehaviour
+{
+
+    public Transform[] neighbours;
+
+    [HideInInspector]
+    public int defense = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set up defense based on the name´s last character
+        defense = int.Parse(gameObject.name.Substring(gameObject.name.Length - 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
